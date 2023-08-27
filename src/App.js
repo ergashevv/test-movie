4b8bb3938190d8bca9156ec404e105852b1a6d98
ergashevv <ergashevpulot@gmail.com>
2023-08-27 15:59:09 +0500

    das

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Route, Routes } from "react-router";
 import Home from "./pages/home";
+import Movie from "./pages/movie";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/movie/:id" element={<Home />} />
+        <Route path="/movie/:id" element={<Movie />} />
       </Routes>
     </div>
   );
--- src/pages/movie.jsx
+++ src/pages/movie.jsx
@@ -1,9 +1,36 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 
 const Movie = () => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
   const { id } = useParams();
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/${id}?api_key=41ee00ef54c639e104c9b60ce5d3736b`
+      )
+      .then(function (response) {
+        console.log(response);
+        setData(response?.data);
+        setIsLoading(false);
+      })
+      .catch(function (error) {
+        console.error("Error fetching data:", error);
+        setError(error);
+        setIsLoading(false);
+      });
+  }, []);
+  console.log(data);
+
   return (
     <>
+      <img
+        src={"https://image.tmdb.org/t/p/w500" + data?.backdrop_path}
+        alt="img"
+      />
       <h1>{id}</h1>
     </>
   );
